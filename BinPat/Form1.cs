f5bc3dbe4f95f31904cbe830c13a74d06d4361c0
apocriva <apocriva@gmail.com>
2023-10-04 20:16:01 -0300

    Nice grid, but the data doesn't line up so nicely.

--- BinPat/Form1.cs
+++ BinPat/Form1.cs
@@ -1,14 +1,21 @@
 using System.IO;
+using System.Security.Permissions;
 using System.Text;
 
 namespace BinPat
 {
 	public partial class Form1 : Form
 	{
-		private const int BytesPerRow = 1;
-		private const int BlockLength = 8;
-		private const int BlockOffset = 2;
-		private const bool SkipByte = true;
+		public static readonly int BlockSize = 8;
+		public static readonly int NumColumns = 4;
+
+		public static readonly int[,] Sections = new int[,]
+		{
+			{	// Start	Section Size
+				0x0000,		0,
+				0x0010,		8,
+			}
+		};
 
 		public Form1()
 		{
@@ -26,76 +33,74 @@ namespace BinPat
 
 			textBoxFilename.Text = openFileDialog.FileName;
 			textBoxOutput.Text = string.Empty;
-			try
+
+			StringBuilder sb = new();
+			byte[] buffer;
+			using(BinaryReader reader = new(File.Open(openFileDialog.FileName, FileMode.Open)))
 			{
-				StringBuilder sb = new StringBuilder();
-				byte[] rowBytes = new byte[BytesPerRow];
-				using(BinaryReader reader = new BinaryReader(File.Open(openFileDialog.FileName, FileMode.Open)))
+				buffer = reader.ReadBytes((int)reader.BaseStream.Length);
+			}
+
+			if(buffer == null)
+			{
+				textBoxOutput.Text = "Oops!";
+				return;
+			}
+
+			// Identify the end of meaningful data.
+			int curByte = 0;
+			int lastDataByte = 0;
+			while(curByte < buffer.Length)
+			{
+				byte b = buffer[curByte];
+				if(b != 0)
 				{
-					int curRow = -BlockOffset;
-					int curRowByte = 0;
-					while(reader.BaseStream.Position != reader.BaseStream.Length)
-					{
-						byte b = reader.ReadByte();
-						if(SkipByte)
-						{
-							b = reader.ReadByte();
-						}
-						rowBytes[curRowByte] = b;
-
-						for(int i = 0; i < 8; i++)
-						{
-							int bit = b & 0x01;
-							b >>= 1;
-							sb.Append(bit);
-							sb.Append(',');
-						}
-
-						curRowByte++;
-						if(curRowByte == BytesPerRow)
-						{
-							for(int i = 0; i < BytesPerRow; i++)
-							{
-								char byteChar = '.';
-								if(rowBytes[i] >= 32 && rowBytes[i] <= 127)
-								{
-									byteChar = (char)rowBytes[i];
-								}
-								sb.Append(byteChar);
-								sb.Append(',');
-							}
-							sb.Append(Environment.NewLine);
-							curRowByte = 0;
-
-							curRow++;
-							if(BlockLength > 0 && curRow > 0)
-							{
-								if(curRow % BlockLength == 0)
-								{
-									for(int i = 0; i < BytesPerRow; i++)
-									{
-										for(int j = 0; j < 8; j++)
-										{
-											sb.Append("-,");
-										}
-									}
-									for(int i = 0; i < BytesPerRow; i++)
-									{
-										sb.Append("-,");
-									}
-									sb.Append(Environment.NewLine);
-								}
-							}
-						}
-					}
+					lastDataByte = curByte;
 				}
+				curByte++;
+			}
 
-				textBoxOutput.Text = sb.ToString();
-			}
-			catch(Exception ex)
+			// We take that and halve it because we're dropping what doesn't look good.
+			int pageSize = (lastDataByte / 2 + 1) / NumColumns;
+			byte[] patternData = new byte[pageSize * NumColumns];
+			curByte = 0;
+			while(curByte < patternData.Length)
 			{
-				Console.WriteLine(ex.Message);
+				patternData[curByte] = buffer[curByte * 2 + 1];
+				curByte++;
 			}
+
+			// Output!
+			curByte = 0;
+			int curRow = 0;
+			while(curByte < pageSize)
+			{
+				sb.Append("-,");
+				for(int column = 0; column < NumColumns; column++)
+				{
+					byte b = patternData[curByte + pageSize * column];
+					for(int i = 0; i < 8; i++)
+					{
+						sb.Append(b & 0x01);
+						sb.Append(',');
+						b >>= 1;
+					}
+					sb.Append("-,");
+				}
+				curByte++;
+				sb.Append(Environment.NewLine);
+				curRow++;
+				if(curRow % BlockSize == 0)
+				{
+					for(int i = 0; i < NumColumns * 9 + 1; i++)
+					{
+						sb.Append("-,");
+					}
+					sb.Append(Environment.NewLine);
+				}
+			}
+
+			textBoxOutput.Text = sb.ToString();
 		}
 	}
 }
