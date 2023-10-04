cdf3a5d312828432d4ae664e5709ad53c27b7fd6
apocriva <apocriva@gmail.com>
2023-10-04 20:39:15 -0300

    Preliminary sectioning. Going to split back into columns.

--- BinPat/Form1.cs
+++ BinPat/Form1.cs
@@ -6,15 +6,15 @@ namespace BinPat
 {
 	public partial class Form1 : Form
 	{
-		public static readonly int BlockSize = 8;
-		public static readonly int NumColumns = 4;
-
 		public static readonly int[,] Sections = new int[,]
 		{
-			{	// Start	Section Size
-				0x0000,		0,
-				0x0010,		8,
-			}
+			//	Start		Section Size
+			{   0x0000,     0   }, // Header
+			{   0x0312,     16  }, // Character Sprites
+			{   0x04F2,     0   }, // ???
+			{   0x07A5,     8   }, // Tiles
+			{   0x0845,     0   }, // ???
+			{	0xFFFF,		0	}, // EOF
 		};
 
 		public Form1()
@@ -61,8 +61,7 @@ namespace BinPat
 			}
 
 			// We take that and halve it because we're dropping what doesn't look good.
-			int pageSize = (lastDataByte / 2 + 1) / NumColumns;
-			byte[] patternData = new byte[pageSize * NumColumns];
+			byte[] patternData = new byte[lastDataByte / 2 + 1];
 			curByte = 0;
 			while(curByte < patternData.Length)
 			{
@@ -73,34 +72,58 @@ namespace BinPat
 			// Output!
 			curByte = 0;
 			int curRow = 0;
-			while(curByte < pageSize)
+			int curSection = 0;
+			int curSectionRow = 0;
+			AppendDivider(sb);
+			while(curByte < patternData.Length)
 			{
-				sb.Append("-,");
-				for(int column = 0; column < NumColumns; column++)
+				// Are we into the next section?
+				if(curRow >= Sections[curSection + 1, 0])
 				{
-					byte b = patternData[curByte + pageSize * column];
-					for(int i = 0; i < 8; i++)
-					{
-						sb.Append(b & 0x01);
-						sb.Append(',');
-						b >>= 1;
-					}
-					sb.Append("-,");
+					curSection++;
+					curSectionRow = 0;
+					AppendDivider(sb);
 				}
+				sb.Append("-,");
+				byte b = patternData[curByte];
+				for(int i = 0; i < 8; i++)
+				{
+					sb.Append(b & 0x01);
+					sb.Append(',');
+					b >>= 1;
+				}
+				sb.Append("-,");
+				if(patternData[curByte] >= 0x20 && patternData[curByte] <= 0x7D)
+				{
+					sb.Append((char)patternData[curByte]);
+					sb.Append(',');
+				}
+				else
+				{
+					sb.Append(".,");
+				}
+				sb.AppendFormat("0x{0:X4}", curRow);
 				curByte++;
 				sb.Append(Environment.NewLine);
 				curRow++;
-				if(curRow % BlockSize == 0)
+				curSectionRow++;
+				if(Sections[curSection, 1] > 0 && curSectionRow % Sections[curSection, 1] == 0)
 				{
-					for(int i = 0; i < NumColumns * 9 + 1; i++)
-					{
-						sb.Append("-,");
-					}
-					sb.Append(Environment.NewLine);
+					AppendDivider(sb);
 				}
 			}
+			AppendDivider(sb);
 
 			textBoxOutput.Text = sb.ToString();
+		}
+
+		private void AppendDivider(StringBuilder sb)
+		{
+			for(int i = 0; i < 10; i++)
+			{
+				sb.Append("-,");
+			}
+			sb.Append(Environment.NewLine);
 		}
 	}
 }
