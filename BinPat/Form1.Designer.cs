0d546d5ae65605f84dcb12346dca638a42a99e48
apocriva <apocriva@gmail.com>
2023-10-04 16:49:54 -0300

    First pass.

--- BinPat/Form1.Designer.cs
+++ BinPat/Form1.Designer.cs
@@ -13,7 +13,7 @@
 		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
 		protected override void Dispose(bool disposing)
 		{
-			if (disposing && (components != null))
+			if(disposing && (components != null))
 			{
 				components.Dispose();
 			}
@@ -28,18 +28,116 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			btnLoad = new Button();
+			openFileDialog = new OpenFileDialog();
+			saveFileDialog = new SaveFileDialog();
+			textBoxFilename = new TextBox();
+			textBoxOutput = new TextBox();
+			groupBox1 = new GroupBox();
+			textBoxBytesPerRow = new TextBox();
+			label1 = new Label();
+			groupBox1.SuspendLayout();
 			SuspendLayout();
+			// 
+			// btnLoad
+			// 
+			btnLoad.Location = new Point(12, 188);
+			btnLoad.Name = "btnLoad";
+			btnLoad.Size = new Size(141, 23);
+			btnLoad.TabIndex = 0;
+			btnLoad.Text = "Load";
+			btnLoad.UseVisualStyleBackColor = true;
+			btnLoad.Click += btnLoad_Click;
+			// 
+			// openFileDialog
+			// 
+			openFileDialog.FileName = "file.bin";
+			openFileDialog.Title = "Select Input File...";
+			// 
+			// saveFileDialog
+			// 
+			saveFileDialog.FileName = "output.csv";
+			saveFileDialog.Title = "Save Output...";
+			// 
+			// textBoxFilename
+			// 
+			textBoxFilename.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+			textBoxFilename.Location = new Point(159, 188);
+			textBoxFilename.Name = "textBoxFilename";
+			textBoxFilename.ReadOnly = true;
+			textBoxFilename.Size = new Size(513, 23);
+			textBoxFilename.TabIndex = 1;
+			// 
+			// textBoxOutput
+			// 
+			textBoxOutput.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+			textBoxOutput.Font = new Font("Courier New", 9F, FontStyle.Regular, GraphicsUnit.Point);
+			textBoxOutput.Location = new Point(12, 217);
+			textBoxOutput.Multiline = true;
+			textBoxOutput.Name = "textBoxOutput";
+			textBoxOutput.ReadOnly = true;
+			textBoxOutput.ScrollBars = ScrollBars.Both;
+			textBoxOutput.Size = new Size(660, 361);
+			textBoxOutput.TabIndex = 2;
+			// 
+			// groupBox1
+			// 
+			groupBox1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+			groupBox1.Controls.Add(textBoxBytesPerRow);
+			groupBox1.Controls.Add(label1);
+			groupBox1.Location = new Point(12, 12);
+			groupBox1.Name = "groupBox1";
+			groupBox1.Size = new Size(660, 170);
+			groupBox1.TabIndex = 3;
+			groupBox1.TabStop = false;
+			groupBox1.Text = "Settings";
+			// 
+			// textBoxBytesPerRow
+			// 
+			textBoxBytesPerRow.Location = new Point(176, 19);
+			textBoxBytesPerRow.Name = "textBoxBytesPerRow";
+			textBoxBytesPerRow.Size = new Size(100, 23);
+			textBoxBytesPerRow.TabIndex = 1;
+			textBoxBytesPerRow.Text = "2";
+			// 
+			// label1
+			// 
+			label1.Location = new Point(6, 19);
+			label1.Name = "label1";
+			label1.Size = new Size(164, 23);
+			label1.TabIndex = 0;
+			label1.Text = "Bytes per Row";
+			label1.TextAlign = ContentAlignment.MiddleRight;
 			// 
 			// Form1
 			// 
 			AutoScaleDimensions = new SizeF(7F, 15F);
 			AutoScaleMode = AutoScaleMode.Font;
-			ClientSize = new Size(800, 450);
+			ClientSize = new Size(684, 590);
+			Controls.Add(btnLoad);
+			Controls.Add(textBoxFilename);
+			Controls.Add(groupBox1);
+			Controls.Add(textBoxOutput);
+			FormBorderStyle = FormBorderStyle.FixedDialog;
+			MaximizeBox = false;
 			Name = "Form1";
+			StartPosition = FormStartPosition.CenterScreen;
 			Text = "BinPat";
+			groupBox1.ResumeLayout(false);
+			groupBox1.PerformLayout();
 			ResumeLayout(false);
+			PerformLayout();
 		}
 
 		#endregion
+
+		private Button btnLoad;
+		private OpenFileDialog openFileDialog;
+		private SaveFileDialog saveFileDialog;
+		private TextBox textBoxFilename;
+		private TextBox textBoxOutput;
+		private GroupBox groupBox1;
+		private TextBox textBoxBytesPerRow;
+		private Label label1;
 	}
 }
--- BinPat/Form1.cs
+++ BinPat/Form1.cs
@@ -1,3 +1,6 @@
+using System.IO;
+using System.Text;
+
 namespace BinPat
 {
 	public partial class Form1 : Form
@@ -5,6 +8,80 @@ namespace BinPat
 		public Form1()
 		{
 			InitializeComponent();
+		}
+
+		private void btnLoad_Click(object sender, EventArgs e)
+		{
+			int bytesPerRow = int.Parse(textBoxBytesPerRow.Text);
+			if(bytesPerRow < 1)
+			{
+				bytesPerRow = 1;
+			}
+
+			if(openFileDialog.ShowDialog(this) != DialogResult.OK)
+			{
+				textBoxFilename.Text = string.Empty;
+				textBoxOutput.Text = string.Empty;
+				return;
+			}
+
+			textBoxFilename.Text = openFileDialog.FileName;
+			textBoxOutput.Text = string.Empty;
+			try
+			{
+				StringBuilder sb = new StringBuilder();
+				byte[] rowBytes = new byte[bytesPerRow];
+				using(BinaryReader reader = new BinaryReader(File.Open(openFileDialog.FileName, FileMode.Open)))
+				{
+					int curRowByte = 0;
+					while(reader.BaseStream.Position != reader.BaseStream.Length)
+					{
+						byte b = reader.ReadByte();
+						rowBytes[curRowByte] = b;
+						if(ShouldIncludeByte(curRowByte))
+						{
+							for(int i = 0; i < 8; i++)
+							{
+								int bit = b & 0x01;
+								b >>= 1;
+								sb.Append(bit);
+								sb.Append(',');
+							}
+						}
+
+						curRowByte++;
+						if(curRowByte == bytesPerRow)
+						{
+							for(int i = 0; i < bytesPerRow; i++)
+							{
+								char byteChar = '.';
+								if(rowBytes[i] >= 32 && rowBytes[i] <= 127)
+								{
+									byteChar = (char)rowBytes[i];
+								}
+								if(ShouldIncludeByte(i))
+								{
+									sb.Append(byteChar);
+									sb.Append(',');
+								}
+							}
+							sb.Append(Environment.NewLine);
+							curRowByte = 0;
+						}
+					}
+				}
+
+				textBoxOutput.Text = sb.ToString();
+			}
+			catch(Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
+		}
+
+		private bool ShouldIncludeByte(int curRowByte)
+		{
+			return curRowByte != 0;
 		}
 	}
 }
